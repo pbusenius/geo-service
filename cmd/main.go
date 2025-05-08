ea8af5ecd94f89dc9cdd575300bd5b6aa99d5d90
pbusenius <patrick.busenius@gmail.com>
2025-05-08 17:32:07 +0200

    added simple handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
 
-func main() {}
+
+func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("data")))
+	http.Handle("/static", http.FileServer(http.Dir("static")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
